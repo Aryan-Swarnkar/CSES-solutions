99cef6b1deffa572adf643846e000d17e64d73f6
Aryan Swarnkar <aryanswarnkar596@gmail.com>
2026-09-22 22:58:22 +0530

    revised grid_path_description problem.

--- grid_path_description.cpp
+++ grid_path_description.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 string s;
+
 bool visited[7][7];
 
 int dr[] = {1, -1, 0, 0};
@@ -20,10 +21,10 @@ long long dfs(int r, int c, int step){
     return 0;
   }
 
-  bool up = (r == 0 || visited[r - 1][c]);
-  bool down = (r == 6 || visited[r + 1][c]);
-  bool left = (c == 0 || visited[r][c - 1]);
-  bool right = (c == 6 || visited[r][c + 1]);
+  bool up = (r == 0 || visited[r-1][c]);
+  bool down = (r == 6 || visited[r+1][c]);
+  bool left = (c == 0 || visited[r][c-1]);
+  bool right = (c == 6 || visited[r][c+1]); 
 
   if(up && down && !left && !right){
     return 0;
@@ -35,28 +36,27 @@ long long dfs(int r, int c, int step){
 
   long long ways = 0;
 
-  for(int k = 0;k < 4;k++){
+  for(int i = 0;i < 4;i++){
 
-    if(s[step] != '?' && s[step] != dir[k]){
+    if(s[step] != '?' && s[step] != dir[k])
+      continue;
+
+    int nr = r + dr[i];
+    int nc = c + dc[i];
+
+    if(nr < 0 || nr >= 7 || nc >= 7 && nc < 0){
       continue;
     }
 
-    int nr = r + dr[k];
-    int nc = c + dc[k];
-
-    if(nr < 0 || nr >= 7 || nc < 0 || nc >= 7){
+    if(visited[nr][nc])
       continue;
-    }
-
-    if(visited[nr][nc]){
-      continue;
-    }
 
     visited[nr][nc] = true;
 
     ways += dfs(nr, nc, step + 1);
 
     visited[nr][nc] = false;
+    
   }
 
   return ways;
@@ -67,7 +67,7 @@ int main(){
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  cin >> s;
+  cin>>s;
 
   visited[0][0] = true;
 
